d6e5423947eacfa74743c767a201106f2a584fba
0BAB1 <hb.hugobabin@gmail.com>
2023-08-20 18:30:30 +0200

    take None in account bruh

--- PdfMmtExtractData-main/convert.py
+++ PdfMmtExtractData-main/convert.py
@@ -2,6 +2,7 @@ def toCsv(data : list) -> None:
     #turns data into csv
     with open("data.csv", "w+") as csvFile:
         for line in data:
-            #get a nice string
-            line = ";".join(line)
+            #get a nice string (clean "none items and replace with 0")
+            cleaned_line = [item if item is not None else "/" for item in line]
+            line = ";".join(cleaned_line)
             csvFile.write(line)
